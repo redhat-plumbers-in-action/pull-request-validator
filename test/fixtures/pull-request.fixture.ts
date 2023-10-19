2e1f4fc688631f2d35e8867883876c6b671e7170
Jan Macku <jamacku@redhat.com>
2023-10-19 13:15:17 +0200

    test: add tests for check runs

--- test/fixtures/pull-request.fixture.ts
+++ test/fixtures/pull-request.fixture.ts
@@ -2,17 +2,15 @@ import { getOctokit } from '../../src/octokit';
 import { PullRequest } from '../../src/pull-request';
 
 export interface PullRequestTestContext {
-  pullRequests: PullRequest[];
+  pullRequests: PullRequest;
 }
 
 export const pullRequestContextFixture: PullRequestTestContext = {
-  pullRequests: [
-    new PullRequest(
-      1,
-      'bfecee1edf7970c1d7704a7cef2523e3917f908e',
-      'redhat-plumbers-in-action',
-      'pull-request-validator',
-      getOctokit('token')
-    ),
-  ],
+  pullRequests: new PullRequest(
+    1,
+    'bfecee1edf7970c1d7704a7cef2523e3917f908e',
+    'redhat-plumbers-in-action',
+    'pull-request-validator',
+    getOctokit('token')
+  ),
 };
