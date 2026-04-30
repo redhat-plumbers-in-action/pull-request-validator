0dd5471cd843502be805639c092cb0cb2ff43ebb
Jan Macku <jamacku@redhat.com>
2026-04-30 14:48:17 +0200

    deps: update dependencies

--- dist/octokit.d.ts
+++ dist/octokit.d.ts
@@ -1,5 +1,5 @@
 import { Octokit } from '@octokit/core';
-declare const CustomOctokit: typeof Octokit & import("@octokit/core/dist-types/types").Constructor<import("@probot/octokit-plugin-config").API & {
+declare const CustomOctokit: typeof Octokit & import("@octokit/core/types").Constructor<import("@probot/octokit-plugin-config").API & {
     paginate: import("@octokit/plugin-paginate-rest").PaginateInterface;
 }>;
 export type CustomOctokit = InstanceType<typeof CustomOctokit>;
